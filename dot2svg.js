769d7103ed35afc06c9e292c29b73284142b6b54
Antony <akb@akb.me.uk>
2018-05-26 17:35:32 +0100

    Workaround.  GraphViz crashes if dot file is too complex. https://gitlab.com/graphviz/graphviz/issues/1127

--- dot2svg.js
+++ dot2svg.js
@@ -5,6 +5,7 @@
 var inDir = __dirname + '/output/dot/';
 var outDir = __dirname + '/output/svg/';
 var nConcurrent = 8;
+var nMaxDotFileSize = 1024 * 1024; // https://gitlab.com/graphviz/graphviz/issues/1127
 
 // End of configuration
 
@@ -16,25 +17,36 @@ fs.readdir(inDir, (err, files) => {
   function convert() {
     if (files.length) {
       var dotfile = files.shift();
-      var arr = dotfile.split('.');
-      arr[arr.length - 1] = 'svg';
-      var svgfile = arr.join('.');
-      var cmd = ['dot ', inDir, dotfile, ' -Tsvg -o ', outDir, svgfile].join('');
-      console.log(cmd);
-      exec(cmd, (error, stdout, stderr) => {
+
+      fs.stat(inDir + dotfile, (error, stats) => {
         if (error) {
           console.log(error);
           process.exit(1);
-        }
-        if (stdout) {
-          console.log(stdout);
-        }
-        if (stderr) {
-          console.log(stderr);
-          process.exit(1);
-        }
-
-        convert();
+        } else if (stats.size > nMaxDotFileSize) {
+          console.log(['Skipping ', dotfile, 'max size exceeded'].join(''));
+          convert();
+        } else {
+          var arr = dotfile.split('.');
+          arr[arr.length - 1] = 'svg';
+          var svgfile = arr.join('.');
+          var cmd = ['dot ', inDir, dotfile, ' -Tsvg -o ', outDir, svgfile].join('');
+    
+          exec(cmd, (error, stdout, stderr) => {
+            if (error) {
+              console.log(error);
+              process.exit(1);
+            }
+            if (stdout) {
+              console.log(stdout);
+            }
+            if (stderr) {
+              console.log(stderr);
+              process.exit(1);
+            }
+    
+            convert();
+          });
+        } 
       });
     }
   }
